200e2ccdfeeffe1ff44dc8cc4b23ba49124d31ed
Alexandr Logunov <logunov1995@gmail.com>
2023-02-16 01:15:15 +0400

    clean-up

--- OOP/Laboratory-Works/src/test/first/Lab1_Task1.java
+++
@@ -1,9 +0,0 @@
-package test.first;
-
-public class Lab1_Task1 {
-    public static void main(String[] args) {
-        for (String arg : args) {
-            System.out.println(arg);
-        }
-    }
-}
